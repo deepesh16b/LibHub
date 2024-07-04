5febdc89c2f8e74eb309d2a2aa6bcc266f74883d
bellacaio16 <bellacaio16@gmail.com>
2024-07-04 13:36:32 +0530

    10

--- server/controller/payment-controller.js
+++ server/controller/payment-controller.js
@@ -28,22 +28,18 @@ export const checkout = async (req, res) => {
 };
 
 export const paymentVerification = async (req, res) => {
-  const {razorpay_payment_id,razorpay_order_id,razorpay_signature} = req.body;
-  const order_id = razorpay_order_id;
+  const { razorpay_payment_id, razorpay_order_id, razorpay_signature } = req.body;
+  const { product_id } = req.query; // Extract product_id from query parameters
 
-  const generated_signature = hmac_sha256(order_id + "|" + razorpay_payment_id, process.env.RAZORPAY_API_SECRET);
+  console.log(req.body);
 
-  if (generated_signature == razorpay_signature) {
-    
+  const generated_signature = hmac_sha256(razorpay_order_id + "|" + razorpay_payment_id, process.env.RAZORPAY_API_SECRET);
 
-    res.redirect(`${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`)
-  }
-  else{
+  if (generated_signature.toString() === razorpay_signature) {
+    res.redirect(`${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}&product_id=${product_id}`);
+  } else {
     res.status(400).json({
       success: false,
     });
   }
-
-
-  
 };
